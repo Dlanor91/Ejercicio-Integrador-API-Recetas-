94a27a716f3d3e9bb18b5e461e49c9a4a4d5aa98
Ronald Lima Nunez <limaronald926@gmail.com>
2022-02-15 19:42:38 -0300

    code aplicado a mapas

--- js/codigo.js
+++ js/codigo.js
@@ -1,138 +1,271 @@
-document.querySelector("#btnRegistro").addEventListener("click",Registro)
+document.querySelector("#btnRegistro").addEventListener("click", Registro);
+document.querySelector("#btnInicioSesion").addEventListener("click", IniciarSesion);
+let menu = document.getElementById("Menu");
+let ruteo = document.getElementById("miRuteo");
+ruteo.addEventListener("ionRouteWillChange", QueMostrar);
+document.querySelector("#Volver").addEventListener("click", VolveraPaginaAnterior);
+let primerIngreso = false;
+let latitudUsuario;
+let longitudUsuario;
+let map;
+let direccionUsuario;
 
-function Registro(){
-    //Faltan las validaciones
-    let nombre = document.querySelector("#name").value.trim();
-    let apellido = document.querySelector("#apellido").value.trim();    
-    let email = document.querySelector("#email").value.trim();
-    let address = document.querySelector("#address").value.trim();
-    let pasw = document.querySelector("#pasw").value.trim();
-
-    let datos = {
-        "nombre":nombre,
-        "apellido":apellido,
-        "email": email,
-        "direccion":address,
-        "password":pasw
-        }
-
-
-    fetch("https://recetas-api-taller.herokuapp.com/api/usuarios",
-    {
-        method: "POST",             //los parametros siempre viajan en el body
-        body:JSON.stringify(datos),  //
-        headers:{
-            "content-type":"application/json"
-        }
-
-    })
-    .then(function (response){        
-        if(response.status !=200){
-            throw new Error("Los datos no son correctos");
-        }
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data);        
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+function VolveraPaginaAnterior() {
+    ruteo.back();
 }
 
-document.querySelector("#btnSesion").addEventListener("click",iniciarSesion)
+navigator.geolocation.getCurrentPosition(GuardarPosicionUsuario, MostrarErrorUbicacion);
 
-function iniciarSesion(){
-    //Faltan las validaciones       
-    let emailSesion = document.querySelector("#emailSesion").value.trim();    
-    let paswSesion = document.querySelector("#paswSesion").value.trim();
-
-    let datos = {        
-        "email": emailSesion,        
-        "password":paswSesion
-        }
-
-
-    fetch("https://recetas-api-taller.herokuapp.com/api/usuarios/session",
-    {
-        method: "POST",             //los parametros siempre viajan en el body
-        body:JSON.stringify(datos),  //
-        headers:{
-            "content-type":"application/json"
-        }
-
-    })
-    .then(function (response){        
-        if(response.status != 200){
-            throw new Error("Los datos no son correctos");
-        }
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data);
-        localStorage.setItem("token",data.token)            //aqui almaceno en el navegador    
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+function GuardarPosicionUsuario(position) {
+    latitudUsuario = position.coords.latitude;
+    longitudUsuario = position.coords.longitude;
+}
+function MostrarErrorUbicacion(error) {
+    console.log(error);
+}
+function closeMenu() {
+    menu.close();
 }
 
-let menu = document.querySelector("#menu"); //aqui solo hago un puntero al elemento pero no hace nada, solo escucha al elemento
 
-function closeMenu(){
-    menu.close();//esto cierra el menu
+function loadRecetas() {
+    fetch("https://recetas-api-taller.herokuapp.com/api/recetas",
+        {
+            headers:
+            {
+                "x-auth": localStorage.getItem("token")
+            }
+        }
+    )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let datos = "";
+            for (let i = 0; i < data.length; i++) {
+
+                datos += `<ion-card><img src="${data[i].urlImagen}"/><ion-card-header><ion-card-subtitle>${data[i].nombre}</ion-card-subtitle><ion-card-title>${data[i].nombre}</ion-card-title>`;
+                datos += `</ion-card-header><ion-card-content>${data[i].nombre}`;
+                datos += `<ion-button onclick="MostrarDetalle('${data[i]._id}')">Detalle</ion-button></ion-card-content></ion-card>`;
+            }
+            document.querySelector("#recetas").innerHTML = datos;
+        })
+        .catch(function (error) {
+            console.log(error.message);
+        })
 }
 
-/* Evento de navegacion */
-document.querySelector("#miRuteo").addEventListener("ionRouteWillChange", mostrarNavegacion)
+function MostrarDetalle(idReceta) {
+    document.querySelector("#detalleReceta").innerHTML = "";
+    console.log("El id de receta es " + idReceta);
+    fetch("https://recetas-api-taller.herokuapp.com/api/recetas/" + idReceta,
+        {
+            headers: {
+                "x-auth": localStorage.getItem("token"),
+                "content-type": "application/json"
+            },
 
-function mostrarNavegacion(event){   //event detecta los cambios y ahi tiene su funcion de callback y el componente y ahi se ejecuta
-    let paginas = document.querySelectorAll(".pagina")  //en este caso son clases por eso son .Algo
-    for(let i=0; i<paginas.length;i++){
+
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let ingredientes = data.ingredientes;
+            direccionUsuario = data.usuario.direccion;
+            let item = document.createElement("ion-item");
+            let itemLabel = document.createElement("ion-label");
+            let parrafo = document.createElement("p");
+            let texto = document.createTextNode(ingredientes);
+            parrafo.appendChild(texto);
+            itemLabel.appendChild(parrafo);
+            item.appendChild(itemLabel);
+
+
+
+            document.querySelector("#detalleReceta").appendChild(item);
+            document.querySelector("#pagina-DetalleReceta").style.display = "block";
+
+            let mapa = document.createElement("div");
+            mapa.setAttribute("id", "map");
+            mapa.style.height = "180px";
+
+            document.querySelector("#detalleReceta").appendChild(mapa);
+
+            if (map != null) {
+                map.remove();
+            }
+            map = L.map('map').setView([latitudUsuario, longitudUsuario], 13);
+        
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                maxZoom: 18,
+                tileSize: 512,
+                zoomOffset: -1,
+            }).addTo(map);
+        
+            L.marker([latitudUsuario, longitudUsuario]).addTo(map)
+                .bindPopup('Mi posición')
+                .openPopup();
+        
+            agregarMarcadorUsuarioReceta(direccionUsuario);
+            setTimeout(function () {
+                let routeDetalle = document.createElement("ion-route");
+                routeDetalle.setAttribute("url", "/Detalle-Receta");
+                routeDetalle.setAttribute("component", "pagina-DetalleReceta");
+                ruteo.appendChild(routeDetalle);
+                ruteo.push("/Detalle-Receta");
+
+                console.log("La latitud del usuario es " + latitudUsuario);
+                console.log("La longitud es" + longitudUsuario);
+            }, 500);
+
+
+
+        })
+}
+
+function agregarMarcadorUsuarioReceta(direccion) {
+
+    console.log(direccion);
+    fetch("https://nominatim.openstreetmap.org/search?&street=Cuareim&city=Montevideo&country=Uruguay&format=json")
+
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            let latitudeUsuarioReceta = data[0].lat;
+            let longitudUsuarioReceta = data[0].lon;
+
+            L.marker([latitudeUsuarioReceta, longitudUsuarioReceta]).addTo(map)
+                .bindPopup('Posicion del usuario de la receta')
+                .openPopup();
+
+            let distancia = map.distance([latitudUsuario, longitudUsuario], [latitudeUsuarioReceta, longitudUsuarioReceta]);
+            console.log("La distancia es" + distancia);
+
+        })
+}
+
+
+function QueMostrar(event) {
+    console.log(event.detail);
+    let paginas = document.getElementsByClassName("pagina");
+    for (let i = 0; i < paginas.length; i++) {
         paginas[i].style.display = "none";
     }
-    if(event.detail.to==="/"){      //este evento tambien te trae el detalle de todo el elemento y lo hago por puntero
-        document.querySelector("#pagina-Registro").style.display="block";
-    }else if(event.detail.to==="/Login"){
-        document.querySelector("#pagina-Login").style.display="block";
-    }else if(event.detail.to==="/Recetas"){
-        document.querySelector("#pagina-Recetas").style.display="block";
+    let paginaMostrar = event.detail.to;
+
+    if (localStorage.getItem("token") != null && !primerIngreso) {
+        loadRecetas();
+        paginaMostrar = "/Recetas";
+        primerIngreso = true;
     }
+
+    if (paginaMostrar === "/") {
+        document.querySelector("#pagina-Registro").style.display = "block";
+    }
+    else if (paginaMostrar === "/Login") {
+        document.querySelector("#pagina-Login").style.display = "block";
+    }
+    else if (paginaMostrar === "/Recetas") {
+        document.querySelector("#pagina-Recetas").style.display = "block";
+    }
+    else if (paginaMostrar === "/Detalle-Receta") {       
+        document.querySelector("#pagina-DetalleReceta").style.display = "block";
+    }
+    else if(paginaMostrar==="/Logout"){
+        localStorage.clear();
+        document.querySelector("#pagina-Login").style.display = "block";
+    }
+
 }
 
-/* API  */
-document.querySelector("#pagina-Recetas").addEventListener("click",mostrarRecetas)
+function Registro() {
+    //faltan agregar validaciones
+    let nombre = document.querySelector("#nombre").value.trim();
+    let apellido = document.querySelector("#apellido").value.trim();
+    let direccion = document.querySelector("#direccion").value.trim();
+    let email = document.querySelector("#email").value.trim();
+    let password = document.querySelector("#password").value.trim();
+    let datos = {
+        "nombre": nombre,
+        "apellido": apellido,
+        "direccion": direccion,
+        "email": email,
+        "password": password
+    }
+    fetch("https://recetas-api-taller.herokuapp.com/api/usuarios",
+        {
+            method: "POST",
+            body: JSON.stringify(datos),
+            headers: {
+                "content-type": "application/json"
+            }
 
-function mostrarRecetas(){
-    fetch("https://recetas-api-taller.herokuapp.com/api/recetas",{
-        method: "Get",            //los parametros siempre viajan en el body        
-        headers:{
-            "x-auth":localStorage.getItem("token") //Aqui recupero el valor
-        }
-    })
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        //console.log(data);
-        data.forEach(function(recetas) {
-            document.querySelector("#mostrarRecetas").innerHTML +=`
-            <ion-card id="mostrarRecetasTodas">                               
-                             
-            <img src="${recetas.urlImagen}" />
-                                <ion-card-header>
-                                  <ion-card-subtitle>Destination</ion-card-subtitle>
-                                  <ion-card-title>${recetas.nombre}</ion-card-title>
-                                </ion-card-header>
-                                <ion-card-content>
-                                  ${recetas.preparacion}
-                                </ion-card-content>
-                                </ion-card>
-            `;
-        });
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+        })
+        .then(function (response) {
+            console.log("Los datos de la primera promise es " + response.status);
+            if (response.status !== 200) {
+                throw new Error("Los datos no son correctos");
+            }
+            return response.json();
+
+        })
+        .then(function (datos) {
+            console.log("Los datos de la segunda promise es " + datos.error);
+            presentToast();
+
+        })
+        .catch(function (error) {
+            presentToast(error.message);
+        })
+
+}
+function presentToast(error) {
+    const toast = document.createElement('ion-toast');
+    toast.message = error;
+    toast.duration = 2000;
+    toast.color = "warning";
+    document.body.appendChild(toast);
+    return toast.present();
+}
+function IniciarSesion() {
+    let email = document.querySelector("#emailSesion").value.trim();
+    let password = document.querySelector("#passwordSesion").value.trim();
+    let datos = {
+        "email": email,
+        "password": password
+    }
+    fetch("https://recetas-api-taller.herokuapp.com/api/usuarios/session",
+        {
+            method: "POST",
+            body: JSON.stringify(datos),
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            localStorage.setItem("token", data.token);
+            presentAlert();
+        })
+        .catch(function (error) {
+            console.log(error.message);
+        })
+
+}
+async function presentAlert() {
+    const alert = document.createElement('ion-alert');
+    alert.cssClass = 'my-custom-class';
+    alert.message = 'El usuario inicio sesión correctamente.';
+    alert.buttons = ['OK'];
+    document.body.appendChild(alert);
+    await alert.present();
+
 
 }
